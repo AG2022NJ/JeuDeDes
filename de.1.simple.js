72abe6e88563ed68236aebe88b298ab2b5790cb6
AG2022Nj <sandy.guillot@hotmail.fr>
2022-11-14 20:26:55 +0100

    Ajout du bouton Ajouter

--- de.1.simple.js
+++ de.1.simple.js
@@ -14,17 +14,29 @@ const lancerDe = function () {
 const clickSurBouton = function() {
     //Lancer le dé virtuel et récupérer le résultat
     const resultat = lancerDe()
-
+    // Afficher le résultat dans le container
+    const container = document.getElementById('container')
+    // Ajouter du Texte au container
+    container.textContent = resultat
     // Récupérer l'image
     const image = document.getElementById('image-de')
     // Ajouter l'attribut src avec le résultat
     image.src = 'media/de' + resultat + '.jpg'
 }
+
+const clickSurAjouter = function() {
+//Ajouter le résultat au score
+const ajouterscore = document.getElementById('ajouterscore')
+ajouterscore.textContent = 0
+
+}
 //Récupérer le bouton
 const buttonLancer = document.getElementById('bouton-lancer')
+const ajouter = document.getElementById('ajouter')
 
 // Executer le lancer de dé lors du click
 buttonLancer.addEventListener('click', clickSurBouton)
+ajouter.addEventListener('click', clickSurAjouter)
 
 
 
